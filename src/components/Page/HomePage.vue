89b6b885532fadd4841cbc6ced59a239e4fd3532
harijaona <harijaona@expat.com>
2023-10-16 22:28:45 +0300

    lottie integration

--- src/components/Page/HomePage.vue
+++ src/components/Page/HomePage.vue
@@ -5,6 +5,7 @@
 			I designed this app using Vue 3, Vuex 4 and VueRouter4. Please feel free to
 			ask me further information aboaut it, I remain available for any questions !
 		</p>
+		<Vue3Lottie :animationData="lottieHome" :height="200" />
 		<router-link to="/tasks">
 			<button class="home-page__link" title="Watch ongoing tasks">
 				Get started !
@@ -12,3 +13,17 @@
 		</router-link>
 	</div>
 </template>
+
+<script>
+import lottieHome from "@/assets/lottie-home.json";
+import { Vue3Lottie } from "vue3-lottie";
+
+export default {
+	components: { Vue3Lottie },
+	data() {
+		return {
+			lottieHome,
+		};
+	},
+};
+</script>
