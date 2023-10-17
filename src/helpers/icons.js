d36b47812019ee18c6f12466f20969fb63895c72
harijaona <harijaona@expat.com>
2023-10-17 09:13:29 +0300

    move icons to dedicated file

---
+++ src/helpers/icons.js
@@ -0,0 +1,30 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+	faTrashAlt,
+	faPlusCircle,
+	faList,
+	faTimesCircle,
+	faHome,
+	faPencilAlt,
+	faEnvelope,
+	faUser,
+	faMessage,
+	faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
+
+/**
+ * Import all the icons we use in library
+ */
+
+library.add(
+	faTrashAlt,
+	faPlusCircle,
+	faList,
+	faTimesCircle,
+	faHome,
+	faPencilAlt,
+	faEnvelope,
+	faUser,
+	faMessage,
+	faPaperPlane
+);
--- src/main.js
+++ src/main.js
@@ -1,36 +1,10 @@
 import "@/styles/index.scss";
 import { createApp } from "vue";
 import App from "@/App.vue";
-// import HomePage from "@/HomePage";
 import store from "@/store/tasks";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-	faTrashAlt,
-	faPlusCircle,
-	faList,
-	faTimesCircle,
-	faHome,
-	faPencilAlt,
-	faEnvelope,
-	faUser,
-	faMessage,
-	faPaperPlane,
-} from "@fortawesome/free-solid-svg-icons";
 import router from "@/helpers/router.js";
-
-library.add(
-	faTrashAlt,
-	faPlusCircle,
-	faList,
-	faTimesCircle,
-	faHome,
-	faPencilAlt,
-	faEnvelope,
-	faUser,
-	faMessage,
-	faPaperPlane
-);
+import "@/helpers/icons.js";
 
 /**
  * Entry point
